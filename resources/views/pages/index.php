ab440255b4fbedf8eab5b777b39224a3cf9ec3e2
Kevin <ducatikevin@yahoo.com>
2021-12-09 02:59:17 -0800

    Cosmetic Update

--- resources/views/pages/index.php
+++ resources/views/pages/index.php
@@ -1,11 +1,13 @@
 <p class="text-center">
-<a href="admin">Admin Login</a> | <a href="about">About</a> | <a href="contact">Contact</a> |
-    <a href="table">HTML Table</a> | <a href="register">Form</a> | <a href="flash">Flash</a>
+    <a href="admin">Admin Login</a> | <a href="about">About</a> | <a href="contact">Contact</a> |<a href="table">HTML Table</a> | <a href="register">Form</a> | <a href="flash">Flash</a>
 </p>
 
+<div class="text-center">
+    <h1>Perfect App Framework</h1>
 
-<h1>Perfect App Framework</h1>
-<h2>A Proprietary Non-Opinionated Framework Implementing Best Practices</h2>
+    <div class="text-center"><h2>A Proprietary Non-Opinionated Framework
+            Implementing Best Practices</h2>
 
-<h3>Contributors</h3>
-Kevin Rubio<br>John Betong
+        <h3>Contributors</h3>
+        <cite>Kevin Rubio<br>John Betong</cite>
+    </div>
